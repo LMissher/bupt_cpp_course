835a11a877fcc125487f1118bfa2076a9659b4cd
morningstarwang <morningstarwang@outlook.com>
2019-11-03 18:21:00 +0800

    refract radix4 code files

--- radix4_search/main.c
+++ radix4_search/main.c
@@ -2,7 +2,66 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include "global.h"
+
+
+#define MAX_LINE 128
+#define WORDS_NUM 986004
+#define PATTERNS_NUM 1270688
+
+typedef struct GS{
+    int memsize;        //内存使用大小
+    double cmpnum;      //比较次数
+    int wordsnum;       //带比较字符串个数
+    int wordsfoundnum;  //查找成功字符串个数
+    int treenodenum;    //树节点个数
+}globalstats;
+
+extern globalstats global_stats;
+void* bupt_malloc(size_t size);
+void *buptCalloc(size_t bytes, size_t size);
+int byte_cmp(char a, char b);
+int cmp(char *ps1, char *ps2, size_t l1, size_t l2);
+
+globalstats global_stats = {0,0,0,0,0};
+
+void* bupt_malloc(size_t size){
+    if (size <= 0) {
+        return NULL;
+    }
+    global_stats.memsize += size;
+    return malloc(size);
+}
+
+void *buptCalloc(size_t bytes, size_t size)
+{
+    if (size <= 0)
+    {
+        return NULL;
+    }
+    global_stats.memsize += bytes * size;
+    return calloc(bytes, size);
+}
+
+int byte_cmp(char a, char b)
+{
+    global_stats.cmpnum++;
+    return a-b;
+}
+
+int cmp(char *ps1, char *ps2, size_t l1, size_t l2)
+{
+    size_t i=0;
+    if (l1 != l2)
+        return 0;
+    for (; i < l1; i++)
+        if (byte_cmp(ps1[i], ps2[i]) != 0)
+            return 0;
+    if (i == l1)
+        return 1;
+    else
+        return 0;
+}
+
 
 typedef struct radix4node {
     uint8_t value;
